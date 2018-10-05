92bb3a391c9342ab90e558878826181ea834a2fe
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-05 16:54:06 +0300

    Initial Commit

---
+++ Log.Pass.Study.Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Log.Pass.Study.Screen
+//
+//  Created by Igor Shelginskiy on 05/10/2018.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
